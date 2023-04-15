5db324017b8583356253881cd624fe10534b21e7
Jose Jaime Come <josejaimecome@gmail.com>
2023-04-15 17:38:06 +0200

    Routers and Links for pages added

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Label from './components/Label';
+import Home from './views/Home';
+import About from './views/About';
 import Login from './views/Login';
+import Label from './components/Label';
 
 
 function App() {
-  return (
-    <div>
+  return (   
+    <Router>
+    <div className="App">
+      <div>
       <Header 
         title="Login and Registration System"
       />
-      <Login />
-      <Footer />
-    </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/About' element={<About />} />
+      </Routes>
+      <Footer />          
+      </div>
+    </div>      
+  </Router> 
 
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,10 +7,28 @@ const Header = ({ title }) => {
   const titleStyle = {
     marginLeft: '6px',
     fontSize: '26px',
-  }
+  };
+  const headerStyle = {
+    with: '100%',
+    textAlign: 'left',
+  };
+  const linkStyle = {
+    paddingRight: '12px',
+    textDecoration: 'none',
+  };
+  const linksStyle = {
+    float: 'right',
+  };
+
   return (
-    <header className='header'>
+    <header style={ headerStyle }>
       <img src="./logo192.png" style={ imgStyle } alt="Logo" /><span style={ titleStyle }>{ title }</span>
+
+      <span style={ linksStyle }>
+        <a style={ linkStyle } href="/login">Login</a>
+        <a style={ linkStyle } href="/create_account">Create Account</a>
+        <a style={ linkStyle } href="/logout">Logout</a>
+      </span>
     </header>
   );
 }
