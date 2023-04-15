0cc5cd687af58f05f76e84db11127bb2e203ecbf
Jose Jaime Come <josejaimecome@gmail.com>
2023-04-15 14:43:20 +0200

    Code for Footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,17 @@
 const Footer = () => {
+  const centerText = {
+    textAlign: 'center',
+  };
+  const divStyle = {
+    width: '100%',
+    backgroundColor: '#EFF1F3',
+    position: 'absolute',
+    bottom: '0px',
+    height: '60px',
+  };
   return (
-    <div>
-      <p>Copyright &copy; 2023</p>       
+    <div style={ divStyle }>
+      <p style={ centerText }>Copyright &copy; 2023</p>       
     </div>
   );
 }
